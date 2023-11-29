fc913aa41a9ce9b3bcac52298ef959f7076217af
andersonSmuniz <anderson.muniz100@hotmail.com>
2023-11-29 08:26:19 -0300

    ajustando tamplates e criando rotas de favoritos

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,6 @@
 from flask import request, jsonify, render_template, url_for, flash, redirect
 from app import app, db, allowed_file
-from sqlalchemy.exc import IntegrityError
-from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy import desc
 from flask_login import login_user, login_required, current_user, logout_user
 from app.models.modelos import Carro, Lance, Usuario, Favorito
 from werkzeug.utils import secure_filename
@@ -9,12 +8,12 @@ import os
 from datetime import datetime
 from flask_bcrypt import Bcrypt
 
+
 bcrypt = Bcrypt(app)
 
 @app.route('/')
 def index():
-    carros = Carro.query.all()
-    
+    carros = Carro.query.filter_by(vendido=False).all()
     return render_template('dashboard.html', carros=carros)
 
 
@@ -73,11 +72,6 @@ def logout():
     return redirect(url_for('login_post'))
 
 
-@app.route('/profile')
-@login_required
-def profile():
-    return render_template('profile.html', name=current_user.nome)
-
 ########### Carro ##############
 
 @app.route('/cadastrar_carro', methods=['GET', 'POST'])
@@ -128,16 +122,19 @@ def atualizar_carro(carro_id):
         flash('Carro não encontrado', 'danger')
         return redirect(url_for('minhas_vendas'))
 
+    if carro.vendido or carro.lances:
+        flash('Não é possível atualizar um carro vendido ou com lances', 'danger')
+        return redirect(url_for('exibir_carro', carro_id=carro_id))
+
     if request.method == 'POST':
         modelo = request.form['modelo']
         ano = request.form['ano']
         nova_imagem = request.files['imagem']
         descricao = request.form['descricao']
         preco = request.form['preco']
-        negociavel = (request.form['negociavel'] == True)
+        negociavel = (request.form['negociavel'] == 'True')  # Converta para booleano
         tempo_duracao = request.form['tempo_duracao']
 
-        print(negociavel) 
         if not carro.usuario_id == current_user.id:
             flash('Você não tem permissão para acessar este carro', 'danger')
             return redirect(url_for('minhas_vendas'))
@@ -156,31 +153,32 @@ def atualizar_carro(carro_id):
         carro.tempo_duracao = tempo_duracao
         db.session.commit()
 
+        flash('Carro atualizado com sucesso', 'success')
         return redirect(url_for('minhas_vendas'))
 
     return render_template('cadastrar_carro.html', carro=carro)
 
-@app.route('/excluir_carro/int:<id>', methods=['GET','POST'])
+
+@app.route('/excluir_carro/<int:carro_id>', methods=['GET'])
 @login_required
-def excluir_carro():
-    if request.method == 'POST':
-        carro_id = request.form['carro_id']
-        
-        carro = Carro.query.get(carro_id)
-        if not carro:
-            return redirect(url_for('minhas_vendas'))
-        
-        if not carro.usuario_id == current_user.id:
-            flash('Você não tem permissão para acessar este carro', 'danger')
-            return redirect(url_for('minhas_vendas'))
-        
-        db.session.delete(carro)
-        db.session.commit()
-        
-        return redirect(url_for('minhas_vendas'))
-    
+def excluir_carro(carro_id):
+    print('aqui')
     carro = Carro.query.get(carro_id)
-    return render_template('excluir_carro.html', carro= carro)
+    if not carro:
+        return redirect(url_for('minhas_vendas'))
+        
+    if carro.vendido or carro.lances:
+        flash('Não é possível excluir um carro vendido ou com lances', 'danger')
+        return redirect(url_for('exibir_carro', carro_id=carro_id))
+
+    if not carro.usuario_id == current_user.id:
+        flash('Você não tem permissão para acessar este carro', 'danger')
+        return redirect(url_for('minhas_vendas'))
+        
+    db.session.delete(carro)
+    db.session.commit()
+        
+    return redirect(url_for('minhas_vendas'))
 
 
 ######### Paginas ###############
@@ -194,6 +192,9 @@ def minhas_vendas():
         return redirect(url_for('login_post'))
     
     carros = Carro.query.filter_by(usuario_id=usuario_id).all()
+
+    for carro in carros:
+        vender_carro(carro)
     
     return render_template('minhas_vendas.html', carros=carros)
 
@@ -203,17 +204,24 @@ def exibir_carro(carro_id):
     carro = Carro.query.get(carro_id)
     if not carro:
         return redirect(url_for('index'))
-    
-    if carro.usuario_id == current_user.id:
-        print(carro_id, Lance.query.filter_by(carro_id=carro_id).all())
-        
-        lances_recebidos = Lance.query.filter_by(carro_id=carro_id).all()
-        return render_template('info.html', carro=carro, usuario_logado=current_user, lances_recebidos=lances_recebidos)
-        
-    else:
-        lances_dados = Lance.query.filter_by(usuario_id=current_user.id).all()    
-        return render_template('info.html', carro=carro, usuario_logado=current_user, lances_dados=lances_dados)
 
+    if carro.usuario_id == current_user.id:
+        lances_recebidos = Lance.query.filter_by(carro_id=carro.id).all()
+
+
+        # Calcular tempo restante
+        tempo_restante = max(0, (carro.tempo_termino - datetime.utcnow()).total_seconds())
+
+        return render_template('info.html', carro=carro, usuario_logado=current_user,
+                               lances_recebidos=lances_recebidos, tempo_restante=tempo_restante, e_favorito=e_favorito(carro))
+    else:
+        lances_dados = Lance.query.filter_by(usuario_id=current_user.id, carro_id=carro.id).all()
+        
+        # Calcular tempo restante
+        tempo_restante = max(0, (carro.tempo_termino - datetime.utcnow()).total_seconds())
+
+        return render_template('info.html', carro=carro, usuario_logado=current_user,
+                               lances_dados=lances_dados, tempo_restante=tempo_restante, e_favorito=e_favorito( carro))
 #######Lance########
 
 @app.route('/realizar_lance/<int:carro_id>', methods=['POST'])
@@ -221,17 +229,28 @@ def exibir_carro(carro_id):
 def realizar_lance(carro_id):
     carro = Carro.query.get(carro_id)
     if not carro:
-        return jsonify({'status': 'error', 'message': 'Carro não encontrado.'}), 404
+        flash('Você não tem permissão para acessar este carro', 'danger')
+        return redirect(url_for('index'))
 
     if not carro.negociavel:
-        return jsonify({'status': 'error', 'message': 'Este carro não é negociável.'}), 400
+        flash('Este carro não é negociável.', 'danger')
+        return redirect(url_for('exibir_carro', carro_id=carro_id))
 
     valor_lance = float(request.form['valor'])
+
+    if valor_lance < carro.preco:
+        flash('O valor do lance deve ser igual ou maior que o valor do carro.', 'danger')
+        return redirect(url_for('exibir_carro', carro_id=carro_id))
+
     novo_lance = Lance(valor=valor_lance, carro=carro, usuario=current_user)
     db.session.add(novo_lance)
     db.session.commit()
 
+    verificar_venda_carro(carro)
+
     return redirect(url_for('exibir_carro', carro_id=carro_id))
+
+
 
 @app.route('/parar_lances/<int:carro_id>', methods=['POST'])
 @login_required
@@ -248,11 +267,13 @@ def parar_lances(carro_id):
 
     return jsonify({'status': 'success', 'message': 'Lances parados com sucesso.'})
 
+
 @app.route('/meus_lances')
 @login_required
 def meus_lances():
     lances_dados = Lance.query.filter_by(usuario_id=current_user.id).all()
     return render_template('lances_dados.html', lances_dados=lances_dados)
+
 
 @app.route('/lances_recebidos')
 @login_required
@@ -278,31 +299,68 @@ def favoritos():
     return render_template('favoritos.html', carros_favoritos=carros_favoritos)
 
 
-@app.route('/adicionar_favorito/<int:carro_id>')
+@app.route('/favoritar_carro/<int:carro_id>')
 @login_required
-def adicionar_favorito(carro_id):
+def favoritar_carro(carro_id):
     usuario_id = current_user.id
     usuario = Usuario.query.get(usuario_id)
     carro = Carro.query.get(carro_id)
-    if carro:
-        usuario.adicionar_favorito(carro)
-        flash('Carro adicionado aos favoritos!', 'success')
-    else:
+
+    if not carro:
         flash('Carro não encontrado.', 'danger')
+    else:
+        if carro in usuario.carros_favoritos():
+            usuario.remover_favorito(carro)
+            flash('Carro removido dos favoritos.', 'success')
+        else:
+            usuario.adicionar_favorito(carro)
+            flash('Carro adicionado aos favoritos!', 'success')
 
     return redirect(url_for('exibir_carro', carro_id=carro_id))
 
 
-@app.route('/remover_favorito/<int:carro_id>')
-@login_required
-def remover_favorito(carro_id):
-    usuario_id = current_user.id
-    usuario = Usuario.query.get(usuario_id)
-    carro = Carro.query.get(carro_id)
-    if carro:
-        usuario.remover_favorito(carro)
-        flash('Carro removido dos favoritos.', 'success')
-    else:
-        flash('Carro não encontrado.', 'danger')
+####funcões###
+def allowed_file(filename):
+    # Adicione sua lógica para verificar se a extensão do arquivo é permitida
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-    return redirect(url_for('exibir_carro', carro_id=carro_id))
+
+def verificar_venda_carro(carro):
+    if not carro.negociavel:
+        carro.vendido = True
+        db.session.commit()
+        flash('O carro foi vendido!', 'success')
+    elif datetime.utcnow() > carro.tempo_termino:
+        melhor_lance = Lance.query.filter_by(carro_id=carro.id, ativo=True).order_by(Lance.valor.desc()).first()
+        if melhor_lance:
+            carro.vendido = True
+            db.session.commit()
+            flash(f'O carro foi vendido para o lance de R$ {melhor_lance.valor}!', 'success')
+            
+
+def vender_carro(carro):
+    if not carro.ativo or carro.vendido:
+        return
+
+    if carro.negociavel:
+        if carro.tempo_termino <= datetime.utcnow():
+            maior_lance = Lance.query.filter_by(carro_id=carro.id, ativo=True).order_by(desc(Lance.valor)).first()
+            if maior_lance:
+                carro.vendido = True
+                carro.tempo_termino = datetime.utcnow()
+                db.session.commit()
+        else:
+            
+            pass
+    else:
+        
+        primeiro_lance = Lance.query.filter_by(carro_id=carro.id, ativo=True).order_by(Lance.tempo_lance).first()
+        if primeiro_lance:
+            carro.vendido = True
+            carro.tempo_termino = datetime.utcnow()
+            db.session.commit()
+            
+
+def e_favorito(carro):
+    return Favorito.query.filter_by(usuario_id=current_user.id, carro_id=carro.id).first() is not None if current_user.is_authenticated else False
